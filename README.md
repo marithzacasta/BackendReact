3292fb46310ed13c65ac85b4137cead2329dbb3c
marithzacasta <marithzacastano9.5@gmail.com>
2025-10-16 00:17:53 -0500

    agregar el README y el archivo de la BD

---
+++ README.md
@@ -0,0 +1,80 @@
+
+# 🎬 CineGalaxy - Backend
+
+Este backend fue desarrollado con **Node.js y Express** para practicar la creación de APIs, la autenticación de usuarios y la conexión con una **base de datos MySQL**.  
+Forma parte del proyecto **CineGalaxy**, que incluye un frontend en React.
+
+## 🚀 Tecnologías utilizadas
+
+- **Node.js + Express**
+- **MySQL + mysql2**
+- **JWT (JSON Web Token)**
+- **Bcrypt**
+- **dotenv**
+- **CORS**
+- **Cookie Parser**
+- **Morgan**
+- **Nodemon (dev)**
+
+## 🧩 Funcionalidades principales
+
+- Registro e inicio de sesión de usuarios.
+
+- Encriptación de contraseñas con Bcrypt.
+
+- Autenticación mediante JWT.
+
+- Conexión y consultas a MySQL.
+
+- Middleware de seguridad con CORS y cookie-parser.
+
+- Registro de peticiones HTTP con Morgan.
+
+
+## 🗄️ Base de datos
+
+El proyecto utiliza MySQL Workbench para gestionar la base de datos.
+
+
+## ▶️ Ejecución
+
+- Clonar el repositorio
+git clone https://github.com/tuusuario/cineapp-backend.git
+
+- Instalar dependencias
+npm install
+
+- Crear el archivo .env en la raíz del proyecto con las siguientes variables
+PORT = 4000
+
+DB_USER  = 'root'
+DB_PASSWORD = '123456789'
+DB_HOST = 'localhost'
+DB_DATABASE = 'manicure'
+DB_PORT = 3306
+
+JWT_SECRET_ACCESS = wkdneoruq230r9y39inf
+JWT_EXPIRE_ACCESS = 1m
+
+JWT_SECRET_REFRESH = nhdblbOUe3i87y3t2728
+JWT_EXPIRE_REFRESH = 7d
+
+NODE_ENV = 'development'
+
+- Ejecutar el servidor
+npm run dev
+
+## 🧠 Aprendizajes obtenidos
+
+Creación de una API REST con Node.js y Express.
+
+Autenticación de usuarios con JWT.
+
+Conexión segura a bases de datos MySQL.
+
+Implementación de buenas prácticas en backend.
+
+
+## 🧑‍💻 Desarrollado por
+
+- [marithzacast] Proyecto creado con fines educativos para practicar desarrollo backend con Node.js y MySQL.
---
+++ src/database/manicure_users.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: manicure
+-- ------------------------------------------------------
+-- Server version	8.0.43
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(500) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Admin','admin@gmail.com','$2b$10$UR2a9zSTqcak0Uedf9FGcOT1SlCNNugT9IkfNSZNO7c0ujcrdTLcq'),(2,'Prueba Perez','prueba@pepe.com','$2b$10$FeqjOOpQGuKcDBeraGYk/uWtm8N5RQehWS9JkDq38DVwOcBwPQNKi'),(3,'sara alzate','sara@gmail.com','$2b$10$.ulEwo.CDuq7N0kEIpOcAesv4y8nBn1iasLH1iDldakx01ztL3g26'),(4,'Pao estrada','pao@gmail.com','$2b$10$w9VcwUd/57zer3rKKwKxmuU0na/ePFsuB6lNBR2fsWKmiEojQo8IS');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-10-15 21:40:13
